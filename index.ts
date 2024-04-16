2d2ad99f5164f8f70f6f8a1b916a81f78dcbdeaf
Hafsa Sultan <you@example.com>
2024-04-16 22:39:38 +0500

    tweak the text and added console at the end

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstnumber" },
-  { message: "Enter second number", type: "number", name: "secondnumber" },
+  { message: "Enter your first number", type: "number", name: "firstnumber" },
+  { message: "Enter your second number", type: "number", name: "secondnumber" },
   {
     message: "Select one of the operators to perform operation",
     type: "list",
@@ -25,3 +25,4 @@ if (answer.operator === "Addition"){
 } else {
     console.log("please select valid operator");
 }
+console.log ("THE END");
